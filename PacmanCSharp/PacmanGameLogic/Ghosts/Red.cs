f825d0b8b9de737eafa7287815050d71767e4f47
mullak99 <mullak99@mullak99.co.uk>
2020-10-10 21:42:59 +0100

    Implemented Moving Target Search for Blinky (Red)
    - Moving Target Search for Blinky (Red)
    - Two movement options, one it a 'true' moving target search, but causes 'bouncy movement', the other is not since it attempts to mitigate the 'bouncy movement'

--- PacmanCSharp/PacmanGameLogic/Ghosts/Red.cs
+++ PacmanCSharp/PacmanGameLogic/Ghosts/Red.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Linq;
 
 namespace Pacman.GameLogic.Ghosts
 {
@@ -34,14 +35,49 @@ namespace Pacman.GameLogic.Ghosts
 			entered = true;
 		}
 
+		bool useCustomMove = true;
+		Direction[] possibleDirections = new Direction[] { Direction.Up, Direction.Right, Direction.Down, Direction.Left };
 		public override void Move() {
-			if( Distance(GameState.Pacman) > randomMoveDist && GameState.Random.Next(0, randomMove) == 0 ) {
-				MoveRandom();
-			} else {
-				MoveAsRed();
+			DirectionDistance[] directions = new DirectionDistance[4];
+
+			for (int i = 0; i < possibleDirections.Length; i++)
+			{
+				Direction d = possibleDirections[i];
+				directions[i] = new DirectionDistance(d, CalculateDistanceToPacman(d));
 			}
+			directions = directions.OrderBy(x => x.Distance).ToArray();
+
+			/* 
+			 * GoDirectionByPreference causes the ghost to have 'bouncy movement' when it gets in a corner. This could be solved, but 
+			 * it wouldnt really be a Moving Target Search since the fastest path would need to be ignored for a few movement cycles.
+			 */
+			if (useCustomMove)
+				GoDirectionByPreference(directions);
+			else
+				MoveInFavoriteDirection(directions[0].Direction, directions[1].Direction, directions[2].Direction, directions[3].Direction);
+
 			base.Move();
 		}
+
+		private float CalculateDistanceToPacman(Direction d)
+        {
+			return DistanceBetweenNode(GameState.Map.GetNode(X, Y).GetNeighbour(d), GameState.Pacman.Node);
+		}
+
+		private float DistanceBetweenNode(Node n1, Node n2)
+        {
+			return (float)Math.Sqrt(Math.Pow(n1.X - n2.X, 2) + Math.Pow(n1.Y - n2.Y, 2));
+		}
+
+		private void GoDirectionByPreference(DirectionDistance[] directionsByPref)
+        {
+			foreach (DirectionDistance d in directionsByPref)
+				if (PossibleDirections().Contains(d.Direction))
+                {
+					NextDirection = d.Direction;
+					return;
+                }
+        }
 
         #region ICloneable Members
 
@@ -60,4 +96,26 @@ namespace Pacman.GameLogic.Ghosts
 
         #endregion
     }
+
+	public class DirectionDistance
+	{
+		Direction _direction;
+		float _distance;
+
+		public DirectionDistance(Direction direction, float distance)
+        {
+			_direction = direction;
+			_distance = distance;
+        }
+
+		public Direction Direction
+        {
+			get { return _direction; }
+        }
+
+		public float Distance
+        {
+			get { return _distance; }
+        }
+	}
 }
